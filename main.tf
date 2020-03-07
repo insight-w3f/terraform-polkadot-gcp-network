c1608fd50e660a093e6d537de580254ff86596d5
Richard Mah <dick.mah@gmail.com>
2020-03-07 16:42:38 -0500

    Add main and variables

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
+data "google_client_config" "current" {}
+
+module "label" {
+  source = "github.com/robc-io/terraform-null-label.git?ref=0.16.1"
+  tags = {
+    NetworkName = var.network_name
+    Owner       = var.owner
+    Terraform   = true
+    VpcType     = "main"
+  }
+
+  environment = var.environment
+  namespace   = var.namespace
+  stage       = var.stage
+}
