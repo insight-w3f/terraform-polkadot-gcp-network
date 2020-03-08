fef5c6a272eb5163b414d335b1d0f980fa48cb0f
Richard Mah <dick.mah@gmail.com>
2020-03-08 18:02:08 -0400

    Fix test files

--- examples/defaults/gcp_provider.tf
+++ examples/defaults/gcp_provider.tf
@@ -6,12 +6,7 @@ variable "gcp_project" {
   default = "project"
 }
 
-variable "gcp_zone" {
-  default = "us-east1-b"
-}
-
 provider "google" {
   project = var.gcp_project
   region  = var.gcp_region
-  zone    = var.gcp_zone
 }
